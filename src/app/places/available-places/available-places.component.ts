579b1d49031ef54fe8e2a12cf974cedc4be3670e
Adepu Prashanth <prashanth737f2@gmail.com>
2026-04-15 23:27:11 +0530

    shown how to handle error, by explictly throwing error in app.js

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -17,15 +17,31 @@ export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
+  isFetching = signal(false);
+  error = signal('');
 
   ngOnInit(): void {
+
+      this.isFetching.set(true);
       // returns Observable
       // unless u call .subscribe on the get request, get call wont do anything
-      const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000')
-      .pipe(map( (responseData) =>responseData.places))
+      const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+      .pipe(map( (responseData) =>responseData.places), catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error('something went wrong while fetching available places. please try again later.'));
+      }
+      ))
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+
+        },
+        complete: () =>
+        {
+          this.isFetching.set(false);
         }
       });
       this.destroyRef.onDestroy(
